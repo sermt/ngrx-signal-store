6277010646d7f3a0b02b16aa6f9359fc38da715a
Kobi Hari <kobi2294@yahoo.com>
2024-11-16 18:34:05 +0200

    beforew quiz app on section 3

---
+++ demo/02. ngrx-quiz/src/app/components/done/done.component.ts
@@ -0,0 +1,18 @@
+import { Component, computed, Input, signal } from '@angular/core';
+import { SharedModule } from '../../shared.module';
+
+@Component({
+  selector: 'app-done',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './done.component.html',
+  styleUrl: './done.component.scss'
+})
+export class DoneComponent {
+  readonly correct = signal(3);
+
+  readonly total = signal(8);
+
+  readonly score = computed(() => this.correct() / this.total());
+
+}
