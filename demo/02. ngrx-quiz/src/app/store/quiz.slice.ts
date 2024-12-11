af848f5f8c16c9308067063f183a683a8871436e
Kobi Hari <kobi2294@yahoo.com>
2024-12-11 15:50:11 +0200

    S03L05 - Added the store state and initial value

---
+++ demo/02. ngrx-quiz/src/app/store/quiz.slice.ts
@@ -0,0 +1,12 @@
+import { QUESTIONS } from "../data/questions";
+import { Question } from "../models/question.model";
+
+export interface QuizSlice {
+    readonly questions: Question[];
+    readonly answers: number[];
+}
+
+export const initialQuizSlice: QuizSlice = {
+    questions: QUESTIONS, 
+    answers: []
+}
