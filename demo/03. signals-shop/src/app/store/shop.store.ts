014fe0779651925c48dbdc4f9f947c9b93224fdd
Kobi Hari <kobi2294@yahoo.com>
2024-12-19 18:47:42 +0200

    S04L07 - updaters

--- demo/03. signals-shop/src/app/store/shop.store.ts
+++ demo/03. signals-shop/src/app/store/shop.store.ts
@@ -1,7 +1,8 @@
-import { signalStore, withComputed, withState } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
 import { initialShopSlice } from './shop.slice';
 import { computed } from '@angular/core';
 import { buildCartVm, buildProductListVm } from './shop-vm.builder';
+import * as updaters from './shop.updaters';
 export const ShopStore = signalStore(
     { providedIn: 'root' },
     withState(initialShopSlice), 
@@ -17,5 +18,21 @@ export const ShopStore = signalStore(
             store.taxRate(), 
             store.cartVisible()
         ))
+    })), 
+    withMethods(store => ({
+        setSearchWord: (searchWord: string) => 
+            patchState(store, updaters.setSearchWord(searchWord)),
+        addToCart: (productId: string) =>
+            patchState(store, updaters.addToCart(productId)),
+        viewCart: () =>
+            patchState(store, updaters.viewCart()),
+        hideCart: () =>
+            patchState(store, updaters.hideCart()),
+        incrementQuantity: (productId: string) =>
+            patchState(store, updaters.incrementQuantity(productId)),
+        decrementQuantity: (productId: string) =>
+            patchState(store, updaters.decrementQuantity(productId)),
+        checkoutCart: () =>
+            patchState(store, updaters.checkoutCart())
     }))
 )
