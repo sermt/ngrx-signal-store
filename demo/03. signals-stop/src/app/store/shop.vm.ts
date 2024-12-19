e1ecd6836de33ccbe240e7d758127cd20d6a15a7
Kobi Hari <kobi2294@yahoo.com>
2024-12-19 17:39:26 +0200

    S04L03 - view models

---
+++ demo/03. signals-stop/src/app/store/shop.vm.ts
@@ -0,0 +1,17 @@
+import { CartItemVm } from "../components/cart/view-model/cart-item.vm";
+import { ProductItemVm } from "../components/items-list/view-model/product-item.vm";
+
+export interface ProductListVm {
+    readonly productItems: ProductItemVm[];
+}
+
+
+export interface CartVm {
+    readonly items: CartItemVm[];
+    readonly subtotal: number;
+    readonly tax: number;
+    readonly total: number;
+    readonly isActive: boolean;
+    readonly isVisible: boolean;
+
+}
