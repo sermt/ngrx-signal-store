5f8732d2bb4697837d6bb3c6d6311482556f5c40
Kobi Hari <kobi2294@yahoo.com>
2025-01-02 14:00:22 +0200

    S06L07 - Refactored Methods

--- demo/03. signals-shop/src/app/features/cart/store/cart.store.ts
+++ demo/03. signals-shop/src/app/features/cart/store/cart.store.ts
@@ -1,14 +1,20 @@
-import { signalStore, withComputed, withProps, withState } from "@ngrx/signals";
+import { signalStore, withComputed, withMethods, withProps, withState } from "@ngrx/signals";
 import { initialCartSlice } from "./cart.slice";
 import { ShopStore } from "../../../store/shop.store";
 import { computed, inject } from "@angular/core";
 import { buildCartVm } from "./cart.vm-builder";
+import { checkoutCart, incrementQuantity } from "../../../store/shop.updaters";
 
 export const CartStore = signalStore(
     withState(initialCartSlice), 
     withProps(_ => ({
         _shopStore: inject(ShopStore)
     })), 
+    withMethods(store => ({
+        incrementQuantity: store._shopStore.incrementQuantity, 
+        decrementQuantity: store._shopStore.decrementQuantity, 
+        checkoutCart: store._shopStore.checkoutCart
+    })),
     withComputed(store => ({
         vm: computed(() => buildCartVm(
             store._shopStore.products(), 
--- demo/03. signals-shop/src/app/features/product-list/store/product-list.store.ts
+++ demo/03. signals-shop/src/app/features/product-list/store/product-list.store.ts
@@ -1,13 +1,18 @@
-import { signalStore, withComputed, withProps, withState } from "@ngrx/signals";
+import { signalStore, withComputed, withMethods, withProps, withState } from "@ngrx/signals";
 import { initialProductListSlice } from "./product-list.slice";
 import { computed, inject } from "@angular/core";
 import { buildProductListVm } from "./product-list.vm-builder";
 import { ShopStore } from "../../../store/shop.store";
+import { addToCart, viewCart } from "../../../store/shop.updaters";
 
 export const ProductListStore = signalStore(
     withState(initialProductListSlice), 
     withProps(_ => ({
         _shopStore: inject(ShopStore)
+    })),
+    withMethods(store => ({
+        addToCart: store._shopStore.addToCart, 
+        viewCart: store._shopStore.viewCart
     })),
     withComputed(store => ({
         vm: computed(() => buildProductListVm(
