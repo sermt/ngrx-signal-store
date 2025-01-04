641caec61fc0ebdeddf594115def2f3e0b1bf021
Kobi Hari <kobi2294@yahoo.com>
2025-01-04 21:41:41 +0200

    S07L06 - side effects in rxMethods

--- demo/02. ngrx-quiz/src/app/store/app.store.ts
+++ demo/02. ngrx-quiz/src/app/store/app.store.ts
@@ -1,24 +1,22 @@
 import {
   patchState,
   signalStore,
-  withComputed,
   withHooks,
   withMethods,
   withProps,
   withState,
 } from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { initialAppSlice } from './app.slice';
-import { computed, inject } from '@angular/core';
-import { DICTIONARIES_TOKEN } from '../tokens/dictionaries.token';
+import { inject } from '@angular/core';
 import {
   changeLanguage,
   resetLanguages,
   setBusy,
   setDictionary,
 } from './app.updaters';
-import { getDictionary } from './app.helpers';
 import { DictionariesService } from '../services/dictionaries.service';
-import { firstValueFrom } from 'rxjs';
+import { delay, firstValueFrom, tap } from 'rxjs';
 
 export const AppStore = signalStore(
   { providedIn: 'root' },
@@ -33,24 +31,27 @@ export const AppStore = signalStore(
     };
   }),
   withMethods((store) => {
-    const _invalidateDictionary = async () => {
-      patchState(store, setBusy(true));
-      const dictionary = await firstValueFrom(
-        store._dictionariesService.getDictionaryWithDelay(
-          store.selectedLanguage()
-        )
-      );
-      patchState(store, setBusy(false), setDictionary(dictionary));
-    };
+    const _invalidateDictionary = rxMethod<string>(input$ => input$.pipe(
+      tap(lang => {
+        console.log('invalidate dictionary for language', lang);
+        patchState(store, setBusy(true));
+      }),
+      delay(1000), 
+      tap(lang => {
+        console.log('completed invalidate dictionary for language', lang);
+        patchState(store, setBusy(false));
+      })
+
+    ))
 
     return {
-      changeLanguage: async () => {
+      changeLanguage: () => {
         patchState(store, changeLanguage(store._languages));
-        await _invalidateDictionary();
+        _invalidateDictionary(store.selectedLanguage());
       },
-      _resetLanguages: async () => {
+      _resetLanguages: () => {
         patchState(store, resetLanguages(store._languages));
-        await _invalidateDictionary();
+        _invalidateDictionary(store.selectedLanguage());
       },
     };
   }),
