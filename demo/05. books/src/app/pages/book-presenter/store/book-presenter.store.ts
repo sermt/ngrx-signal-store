03212d6f6dd13ea74a1672c46a50275f92d361a3
Kobi Hari <kobi2294@yahoo.com>
2025-01-08 11:52:42 +0200

    S08L02 - with Devtools

--- demo/05. books/src/app/pages/book-presenter/store/book-presenter.store.ts
+++ demo/05. books/src/app/pages/book-presenter/store/book-presenter.store.ts
@@ -2,6 +2,7 @@ import { patchState, signalMethod, signalStore, withComputed, withMethods, withS
 import { initialBookPresenterSlice } from './book-presenter.slice';
 import { computed } from '@angular/core';
 import { BOOKS_COLLECTION } from '../../../data/books-collection';
+import { withDevtools } from '@angular-architects/ngrx-toolkit';
 export const BookPresenterStore = signalStore(
     withState(initialBookPresenterSlice), 
     withComputed(store => ({
@@ -10,5 +11,6 @@ export const BookPresenterStore = signalStore(
     withMethods(store => ({
         setBookId: signalMethod<number>(id => 
             patchState(store, { id }))
-    }))
+    })), 
+    withDevtools('book-presenter')
 )
