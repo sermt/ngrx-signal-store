28ddaef078f6b04b83124ba7f7bf5a8eb758e4e0
Kobi Hari <kobi2294@yahoo.com>
2025-01-08 12:08:48 +0200

    S08L03 - withEntities

--- demo/05. books/src/app/pages/book-presenter/store/book-presenter.store.ts
+++ demo/05. books/src/app/pages/book-presenter/store/book-presenter.store.ts
@@ -1,16 +1,24 @@
-import { patchState, signalMethod, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { patchState, signalMethod, signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
 import { initialBookPresenterSlice } from './book-presenter.slice';
 import { computed } from '@angular/core';
 import { BOOKS_COLLECTION } from '../../../data/books-collection';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
+import { setAllEntities, withEntities } from '@ngrx/signals/entities';
+import { Book } from '../../../models/book.model';
 export const BookPresenterStore = signalStore(
     withState(initialBookPresenterSlice), 
+    withEntities<Book>(),
     withComputed(store => ({
-        book: computed(() => BOOKS_COLLECTION[store.id()])
+        book: computed(() => store.entityMap()[store.id()])
     })), 
     withMethods(store => ({
         setBookId: signalMethod<number>(id => 
             patchState(store, { id }))
     })), 
-    withDevtools('book-presenter')
+    withDevtools('book-presenter'), 
+    withHooks(store => ({
+        onInit: () => {
+            patchState(store, setAllEntities(BOOKS_COLLECTION))
+        }
+    }))
 )
