c1ef187b627da49cdd561d1f1a590c8a4e8ba88d
Kobi Hari <kobi2294@yahoo.com>
2025-01-08 12:45:04 +0200

    S08L05 - Modifying the entities

--- demo/05. books/src/app/pages/book-presenter/store/book-presenter.store.ts
+++ demo/05. books/src/app/pages/book-presenter/store/book-presenter.store.ts
@@ -3,7 +3,7 @@ import { initialBookPresenterSlice } from './book-presenter.slice';
 import { computed } from '@angular/core';
 import { BOOKS_COLLECTION } from '../../../data/books-collection';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
-import { entityConfig, setAllEntities, withEntities } from '@ngrx/signals/entities';
+import { entityConfig, setAllEntities, updateEntity, withEntities } from '@ngrx/signals/entities';
 import { Book } from '../../../models/book.model';
 
 const bookConfig = entityConfig({
@@ -21,7 +21,13 @@ export const BookPresenterStore = signalStore(
     })), 
     withMethods(store => ({
         setBookId: signalMethod<number>(id => 
-            patchState(store, { id }))
+            patchState(store, { id })), 
+        renameCurrentBook: (name: string) => {
+            patchState(store, updateEntity({
+                id: store.id(), 
+                changes: { title: name}
+            }, bookConfig))
+        }
     })), 
     withDevtools('book-presenter'), 
     withHooks(store => ({
